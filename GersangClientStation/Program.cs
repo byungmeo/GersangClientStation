76d3c4ab4037f0d08aab123a8621afc2bee6d8a8
byungmeo <wlsrlfvkr@gmail.com>
2021-12-27 15:17:41 +0900

    프로그램 중복 실행 방지

--- GersangClientStation/Program.cs
+++ GersangClientStation/Program.cs
@@ -11,9 +11,18 @@ namespace GersangClientStation {
         /// </summary>
         [STAThread]
         static void Main() {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form_Main());
+            bool flagMutex; //중복 실행 여부 확인
+
+            System.Threading.Mutex mutex = new System.Threading.Mutex(true, "GersangClientStation", out flagMutex);
+            if(flagMutex) {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form_Main());
+                mutex.ReleaseMutex();
+            } else {
+                MessageBox.Show("중복 실행은 불가능 합니다.", "중복 실행", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            
         }
     }
 }
